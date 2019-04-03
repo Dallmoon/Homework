98db133d036b314734e0447f62ef8611a1757a07
Dallmoon <yunhunjun007@gmail.com>
2019-04-03 20:50:24 +0900

    0403 homework

---
+++ JS/main.js
@@ -0,0 +1,32 @@
+$(".menu_item_1").mouseenter(function(){
+    $('.menu_item_1_sub').css("display","flex");
+
+});
+$(".menu_item_1").mouseleave(function(){
+    $('.menu_item_1_sub').css("display","none");
+});
+
+$(".menu_item_2").mouseenter(function(){
+    $('.menu_item_2_sub').css("display","flex");
+
+});
+$(".menu_item_2").mouseleave(function(){
+    $('.menu_item_2_sub').css("display","none");
+});
+
+$(".menu_item_3").mouseenter(function(){
+    $('.menu_item_3_sub').css("display","flex");
+
+});
+$(".menu_item_3").mouseleave(function(){
+    $('.menu_item_3_sub').css("display","none");
+});
+
+$(".menu_item_4").mouseenter(function(){
+    $('.menu_item_4_sub').css("display","flex");
+
+});
+$(".menu_item_4").mouseleave(function(){
+    $('.menu_item_4_sub').css("display","none");
+});
+
