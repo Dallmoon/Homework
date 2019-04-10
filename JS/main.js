33c3c8ab41f44b548cdf9bef48fae74c6e87fc3a
Dallmoon <yunhunjun007@gmail.com>
2019-04-10 17:23:54 +0900

    homework_04-10

--- JS/main.js
+++ JS/main.js
@@ -1,32 +1,82 @@
-$(".menu_item_1").mouseenter(function(){
-    $('.menu_item_1_sub').css("display","flex");
+var icon_down_open = $('.icon-down-open')
 
+
+/*메인 nav 오픈 */
+$(document).ready(function() {
+    $('.menu_item_sub').hide();
+
+    $(".menu_1").hover(
+        function () {
+            $('.menu_item_1_sub').stop().slideDown(300);
+        },
+         function () {
+          $('.menu_item_1_sub').stop().slideUp(300);
+        } 
+      );
+      $(".menu_2").hover(
+        function () {
+            $('.menu_item_2_sub').stop().slideDown(300);
+        },
+         function () {
+          $('.menu_item_2_sub').stop().slideUp(300);
+        } 
+      );
+      $(".menu_3").hover(
+        function () {
+            $('.menu_item_3_sub').stop().slideDown(300);
+        },
+         function () {
+          $('.menu_item_3_sub').stop().slideUp(300);
+        } 
+      );
+      $(".menu_4").hover(
+        function () {
+            $('.menu_item_4_sub').stop().slideDown(300);
+        },
+         function () {
+          $('.menu_item_4_sub').stop().slideUp(300);
+        } 
+      );
 });
-$(".menu_item_1").mouseleave(function(){
-    $('.menu_item_1_sub').css("display","none");
+$('.menu_1').focusin(function(){
+    $('.menu_item_1_sub').show();
+})
+$('.menu_item_1_sub>li:last-child').focusout(function(){
+    $('.menu_item_1_sub').hide();
+})
+$('.menu_2').focusin(function(){
+    $('.menu_item_2_sub').show();
+})
+$('.menu_item_2_sub>li:last-child').focusout(function(){
+    $('.menu_item_2_sub').hide();
+})
+$('.menu_3').focusin(function(){
+    $('.menu_item_3_sub').show();
+})
+$('.menu_item_3_sub>li:last-child').focusout(function(){
+    $('.menu_item_3_sub').hide();
+})
+$('.menu_4').focusin(function(){
+    $('.menu_item_4_sub').show();
+})
+$('.menu_item_4_sub>li:last-child').focusout(function(){
+    $('.menu_item_4_sub').hide();
+})
+
+  
+
+/*언어 선택 창*/ 
+icon_down_open.on('click focusin',function(){
+    $('.language_select').removeClass("language_select_active")
+    $('.language_select').addClass('language_select_active')
+});
+$('.language_select li').on('click' ,function(){
+    $('.language_select').removeClass("language_select_active")    
 });
 
-$(".menu_item_2").mouseenter(function(){
-    $('.menu_item_2_sub').css("display","flex");
 
-});
-$(".menu_item_2").mouseleave(function(){
-    $('.menu_item_2_sub').css("display","none");
+$('.language_select li:last-child').on('focusout' ,function(){
+    $('.language_select').removeClass("language_select_active")    
 });
 
-$(".menu_item_3").mouseenter(function(){
-    $('.menu_item_3_sub').css("display","flex");
-
-});
-$(".menu_item_3").mouseleave(function(){
-    $('.menu_item_3_sub').css("display","none");
-});
-
-$(".menu_item_4").mouseenter(function(){
-    $('.menu_item_4_sub').css("display","flex");
-
-});
-$(".menu_item_4").mouseleave(function(){
-    $('.menu_item_4_sub').css("display","none");
-});
 
